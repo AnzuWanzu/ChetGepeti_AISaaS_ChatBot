f46acbcec6578e82179dcd006db91187eee06737
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-08-27 18:35:24 +0800

    config ts files. Opened express app. Use npm run dev

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 const app = express();
-
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+//# sourceMappingURL=index.js.map
