b6003d83bc8562095582255858a0f42249cbfb0d
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-08-27 22:21:28 +0800

    missed a spot

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,11 +3,13 @@ import { connectDB } from "./db/connection.js";
 const PORT = process.env.PORT;
 //Connections and Listener
 connectDB()
-    .then(() => {
-    app.listen(PORT, () => console.log(`Connected to Server and Database |
-         Listening on port: ${PORT}`));
-})
-    .catch((error) => {
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Connected to Server and Database |
+         Listening on port: ${PORT}`)
+    );
+  })
+  .catch((error) => {
     console.log(error);
-});
+  });
 //# sourceMappingURL=index.js.map
