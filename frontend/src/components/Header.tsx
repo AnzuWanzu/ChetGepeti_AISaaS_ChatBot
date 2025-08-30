d696d8c8c7b47da212d4a6242dc05a53aadd0322
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-08-30 16:36:28 +0800

    Logo component rendered. Inital Auth context created.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,14 @@
 import { AppBar, Toolbar } from "@mui/material";
-import React from "react";
+import Logo from "./shared/Logo.tsx";
 
 const Header = () => {
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
     >
-      <Toolbar sx={{ display: "flex" }}></Toolbar>
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+      </Toolbar>
     </AppBar>
   );
 };
