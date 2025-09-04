ada9bf7f65c6e61d0a41a74974bfb861ccd9d2e3
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-09-04 15:40:39 +0800

    sign up implemented

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -112,8 +112,8 @@ const Chat = () => {
               fontWeight: 700,
             }}
           >
-            {auth?.user?.name[0]}
-            {auth?.user?.name.split(" ")[1][0]}
+            {auth?.user?.name[0]?.toUpperCase()}
+            {auth?.user?.name.split(" ")[1]?.[0]?.toUpperCase()}
           </Avatar>
           <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
             You are talking to a ChatBot
